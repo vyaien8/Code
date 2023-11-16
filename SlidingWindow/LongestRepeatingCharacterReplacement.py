ca69f57df40a4d337e4c3e9e377aeb1a614e8817
vyaien8 <tadangvinh03@gmail.com>
2023-11-16 10:33:24 +0700

    Sliding Window

--- SlidingWindow/LongestRepeatingCharacterReplacement.py
+++ SlidingWindow/LongestRepeatingCharacterReplacement.py
@@ -1,12 +1,14 @@
 class Solution:
     def characterReplacement(self, s, k):
         count = {}
+        res = 0
         l = 0
         maxF = 0 # max frequency character
         for r in range(len(s)):
             count[s[r]] = count.get(s[r], 0) + 1
             maxF = max(maxF, count[s[r]])
-            if (r - l + 1) - maxF > k: # length of window - maxF = number characters that need to replace
-                count[s[l]] -= 1
-                l += 1
-        return (r - l + 1)
+            while (r - l + 1) - maxF > k: # when the number need to replace is bigger than the maximum allowed
+                count[s[l]] -= 1 
+                l += 1 
+            res = max(res, l - r + 1)
+        return res
